5d25079dcbde3463facfde9358a4c90381f28149
Karthik Nayak <nayakkarthik02@gmail.com>
2018-04-07 14:56:28 -0400

    omni

--- background.js
+++ background.js
@@ -14,3 +14,10 @@ chrome.runtime.onInstalled.addListener(function() {
     }]);
   });
 });
+
+chrome.omnibox.onInputEntered.addListener(
+  function(text) {
+    // Encode user input for special characters , / ? : @ & = + $ #
+    var newURL = 'https://www.google.com/search?q=' + encodeURIComponent(text);
+    chrome.tabs.create({ url: newURL });
+  });

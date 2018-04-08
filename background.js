085d19200b31f86d76ec74a1bc87fc3e310961e6
Karthik Nayak <nayakkarthik02@gmail.com>
2018-04-08 04:23:45 -0400

    working1

--- background.js
+++ background.js
@@ -1,9 +1,7 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
+	
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
@@ -14,12 +12,3 @@ chrome.runtime.onInstalled.addListener(function() {
     }]);
   });
 });
-
-chrome.omnibox.onInputEntered.addListener(
-  function(text) {
-    // Encode user input for special characters , / ? : @ & = + $ #
-    var newURL = 'https://www.google.com/search?q=' + encodeURIComponent(text);
-    chrome.tabs.create({ url: newURL });
-  });
-
-string chrome.runtime.getURL(string path)
--- popup.js
+++ popup.js
@@ -2,33 +2,44 @@
 
 var url = ""
 
+// get current tab url
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     url = tabs[0].url;
 });
 
-var x="";
+// call server with url  !!!
+
+// get text from search bar when clicked 
+var search_string ="";
 function get_search_string() {
-    x = document.getElementById("searchbox").value;
-    document.getElementById('demo').textContent = x;
+    search_string = document.getElementById("searchbox").value;
+
+    
+    // send server the search string !!!
+    document.getElementById('demo').textContent = search_string;
+
 }
 
 document.getElementById('search').onclick = get_search_string;
 
-
-function add(type) {
+// dynamically adds butttons
+// button_text = ""
+function add(index, button_text, button_value) {
   //Create an input type dynamically.   
   var element = document.createElement("button");
   var br = document.createElement("br");
+
   //Assign different attributes to the element. 
   element.type = "button";
-  element.value = "typfasfae";
-  element.innerHTML = 'hello';
-  element.name = "button name"; 
+  element.value = button_value;
+  element.innerHTML = button_text;
+  element.name = index; 
+  element.class = "terms"
   element.style.height = "30px";
   element.style.width = "350px";
   element.style.border= "0.5px solid #ff1a1a";
 
-  element.onclick = get_search_string; // this is where you should JUMPPPPP
+  // element.onclick =  button_click(element);// this is where you should JUMPPPPP
 
   // var foo = document.getElementById("resultfield");
   //Append the element in page (in span).  
@@ -37,22 +48,61 @@ function add(type) {
   document.body.appendChild(br);
 }
 
-// document.getElementById("addBtn").onclick = function() {
-//   add("text");
-// };
+
+// document.addEventListener("click", function(e) {
+//   if (!e.target.classList.contains("terms")) {
+//     return;
+//   }
+//   console.log("hi");
+//   var name = e.target.textContent;
+
+
+// });
+
+
+
+var time = ""
+function button_click(element) {
+	console.log(element.value);
+    chrome.tabs.query({url: "*://*.youtube.com/*"}, logTabs);
+    // time = button_value
+    // console.log(time)
+}
+
+
+function logTabs(tabs) {
+  for (let tab of tabs) {
+    // tab.url requires the `tabs` permission
+	seekVideo(tab.id);
+  }
+}
+
 
 var i;
 for (i = 0; i < 10; i++) { 
-    add("hello");
+    add(i, "hello", "3");
 }
 
 // 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+// var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+function seekVideo(tabid){
+
+// var codeToExecute = 'document.querySelector(".ytp-play-button.ytp-button").click()';
+// var codeToExecute = 'document.getElementById("movie_player").seekTo(26)';
+var codeToExecute = "document.querySelector('video').currentTime = 20";
+
+
+var executing = chrome.tabs.executeScript(
+  		tabid,
+  		{code: codeToExecute}
+	);
+//executing.then(onExecuted, onError);
+}
 
 // {
 // 	"3.3"	: "I’ve had my ups and downs",
